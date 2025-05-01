e5653f9eccec7ba96449678d07a6c8b09736d7a4
ayham <[ayham10.aboajeb@gmail.com]>
2025-05-01 17:18:32 +0300

    some editing

--- src/pages/LoadingPage2.jsx
+++ src/pages/LoadingPage2.jsx
@@ -2,12 +2,12 @@
 
 const LoadingPage2 = () => {
     return (
-        <div className="w-full  h-screen flex py-[100px] justify-center items-center gap-[20px]">
+        <div className="w-full h-screen flex flex-col py-[100px] justify-center items-center gap-[20px]">
             <span className="text-[--main-blue] font-medium text-[20px]">Just a moment</span>
-            <div className="flex w-fit items-center justify-center">
-                <span className="w-[20px] h-[20px] rounded-full bg-[--main-blue]"></span>
-                <span className="w-[20px] h-[20px] rounded-full bg-[--main-blue]"></span>
-                <span className="w-[20px] h-[20px] rounded-full bg-[--main-blue]"></span>
+            <div className="flex w-fit items-center justify-center gap-[20px]">
+                <span className="w-[10px] h-[10px] rounded-full bg-[--main-blue]"></span>
+                <span className="w-[10px] h-[10px] rounded-full bg-[--main-blue]"></span>
+                <span className="w-[10px] h-[10px] rounded-full bg-[--main-blue]"></span>
             </div>
 
         </div>
