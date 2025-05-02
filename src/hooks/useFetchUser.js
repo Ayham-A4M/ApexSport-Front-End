343204202734bee2d0bb6b242a93deae7fbd9ff9
ayham <[ayham10.aboajeb@gmail.com]>
2025-05-02 17:20:04 +0300

    some editing

--- src/hooks/useFetchUser.js
+++ src/hooks/useFetchUser.js
@@ -1,17 +1,20 @@
 import getPersonalInformation from "../functions/getPersonalInformation";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
 export function useFetchUser(setIsLoading) {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
-    useEffect(() => {
+    useEffect(()=>{
+        console.log('new user update',user)
+    },[user])
+    useLayoutEffect(() => {
         async function getLogin() {
             try {
                 const res = await getPersonalInformation();
                 console.log(res,'data user')
-                if (res && res.statusText === "OK") { setUser(res.data); res.data.Role === "Admin" ? navigate('/adminPage', { replace: true }) : false }
+                if (res && res.status<250) { setUser(res.data); res.data.Role === "Admin" ? navigate('/adminPage', { replace: true }) : false }
             } catch (err) {
-                console.log('err');
+                console.log('err',err);
             } finally {
                 setIsLoading(false)
             }
