78d15bcf1a216101e3c16f0cd5a10edfef2ac850
ayham <[ayham10.aboajeb@gmail.com]>
2025-05-02 16:59:39 +0300

    some editing

--- src/hooks/useFetchUser.js
+++ src/hooks/useFetchUser.js
@@ -8,7 +8,7 @@ export function useFetchUser(setIsLoading) {
         async function getLogin() {
             try {
                 const res = await getPersonalInformation();
-                console.log(res.data,'data user')
+                console.log(res,'data user')
                 if (res && res.statusText === "OK") { setUser(res.data); res.data.Role === "Admin" ? navigate('/adminPage', { replace: true }) : false }
             } catch (err) {
                 console.log('err');
@@ -19,5 +19,5 @@ export function useFetchUser(setIsLoading) {
         }
         getLogin();
     }, [])
-    
+    return { user, setUser }
 }
